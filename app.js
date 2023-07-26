3c0c22560fd19ce0c2607ac7d1d3b4afade38a7b
Buntu William <williambuntu1@gmail.com>
2023-07-27 00:22:52 +0200

    commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+// const chalk = require ('chalk');
 const add = require('./notes')
 const validator = require('validator');
+
 
 const sum = add(37,36)
 
 console.log(sum)
 
 console.log(validator.isEmail('foo@bar.com'))
-
+// const gg = chalk.warn(validator.isEmail('foo@bar.com'))
+console.log(process.argv)
 
 // fs.writeFileSync('notes.txt', 'My name is Andrew.')
 
